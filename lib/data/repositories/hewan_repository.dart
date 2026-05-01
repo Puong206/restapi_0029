c7ca081c2408f0b579110f0418b8bd8a5b2a0968
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-05-01 23:22:54 +0700

    feat: tambahkan penanganan respons untuk metode getAllHewan

--- lib/data/repositories/hewan_repository.dart
+++ lib/data/repositories/hewan_repository.dart
@@ -14,5 +14,13 @@ class HewanRepository {
       Uri.parse("$_baseUrl/hewan"),
       headers: {'Authorization': 'Bearer $token', 'Accept': 'application/json'},
     );
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> body = jsonDecode(response.body);
+      final List<dynamic> data = body['data'];
+      return data.map((item) => HewanModel.fromJson(item)).toList();
+    } else {
+      throw Exception("Gagal mengambil data hewan: ${response.statusCode}");
+    }
   }
 }
