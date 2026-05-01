6b1ba3393c371ab202e687198e224db720e9380a
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-05-02 00:28:28 +0700

    feat: tambahkan pengelolaan form dan controller di halaman LoginPage

--- lib/ui/pages/login_page.dart
+++ lib/ui/pages/login_page.dart
@@ -7,3 +7,17 @@ class LoginPage extends StatefulWidget {
   State<LoginPage> createState() => _LoginPageState();
 }
 
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  bool _isObscured = true;
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+}
