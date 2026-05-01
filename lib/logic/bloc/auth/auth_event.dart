d2e79bd918511ac5e78094afd74fa0e472257440
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-05-01 23:34:26 +0700

    feat: tambahkan kelas RegisterRequested untuk menangani permintaan registrasi

--- lib/logic/bloc/auth/auth_event.dart
+++ lib/logic/bloc/auth/auth_event.dart
@@ -11,3 +11,8 @@ class LoginRequested extends AuthEvent {
   final String email, password;
   LoginRequested(this.email, this.password);
 }
+
+class RegisterRequested extends AuthEvent {
+  final String name, email, password;
+  RegisterRequested(this.name, this.email, this.password);
+}
