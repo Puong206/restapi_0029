20b7d1690b9bf8a4b30aed92dcd8073e6eef9f68
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-05-01 23:36:58 +0700

    feat: tambahkan properti repository pada kelas AuthBloc

--- lib/logic/bloc/auth/auth_bloc.dart
+++ lib/logic/bloc/auth/auth_bloc.dart
@@ -1,7 +1,12 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:restapi_0029/data/repositories/auth_repository.dart';
 import 'package:restapi_0029/logic/bloc/auth/auth_event.dart';
 import 'package:restapi_0029/logic/bloc/auth/auth_state.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState>{
-  
+  final AuthRepository repository;
+
+  AuthBloc({required this.repository}) : super(AuthInitial()) {
+    
+  }
 }
