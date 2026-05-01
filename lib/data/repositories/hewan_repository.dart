2c05f50d105bc004f4cfef3e12de925ed331d22f
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-05-01 23:18:47 +0700

    feat: tambahkan kelas HewanRepository dengan URL dasar dan penyedia penyimpanan

---
+++ lib/data/repositories/hewan_repository.dart
@@ -0,0 +1,6 @@
+import 'package:restapi_0029/data/providers/storage_provider.dart';
+
+class HewanRepository {
+  final String _baseUrl = "https://ternak-be-production.up.railway.app/api/v1";
+  final StorageProvider storage = StorageProvider();
+}
