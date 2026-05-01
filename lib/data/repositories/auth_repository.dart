f9a95d281bfc4405861f7b41e94b9ee8c8d0ffad
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-05-01 22:58:39 +0700

    feat: tambahkan penyimpanan aman menggunakan FlutterSecureStorage di AuthRepository

--- lib/data/repositories/auth_repository.dart
+++ lib/data/repositories/auth_repository.dart
@@ -1,3 +1,8 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
 class AuthRepository {
   final String baseUrl = "https://ternak-be-production.up.railway.app/api/v1";
+  final _storage = const FlutterSecureStorage();
+
+  
 }
