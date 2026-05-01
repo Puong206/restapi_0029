d9601b0b01f7c8632371ffc3a04264c70d6b57a4
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-05-01 23:07:17 +0700

    feat: tambahkan metode persistToken untuk menyimpan token JWT

--- lib/data/repositories/auth_repository.dart
+++ lib/data/repositories/auth_repository.dart
@@ -4,5 +4,7 @@ class AuthRepository {
   final String baseUrl = "https://ternak-be-production.up.railway.app/api/v1";
   final _storage = const FlutterSecureStorage();
 
-  
+  Future<void> persistToken(String token) async {
+    await _storage.write(key: 'jwt_token', value: token);
+  }
 }
