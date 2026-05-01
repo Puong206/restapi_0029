21615e1f68b7651774691e6591fb3c6e2e262cdf
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-05-01 23:37:53 +0700

    feat: tambahkan logika untuk menangani event AppStarted di AuthBloc

--- lib/logic/bloc/auth/auth_bloc.dart
+++ lib/logic/bloc/auth/auth_bloc.dart
@@ -7,6 +7,13 @@ class AuthBloc extends Bloc<AuthEvent, AuthState>{
   final AuthRepository repository;
 
   AuthBloc({required this.repository}) : super(AuthInitial()) {
-    
+    on<AppStarted>((event, emit) async {
+      final token = await repository.getToken();
+      if (token != null) {
+        emit(Authenticated(token));
+      } else {
+        emit(Unauthenticated());
+      }
+    });
   }
 }
