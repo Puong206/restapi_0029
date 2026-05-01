0f531f9aa9d5eb5373a1a0235dd6af0030df12ba
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-05-01 22:46:38 +0700

    feat: tambahkan model HewanModel dengan atribut dan konstruktor

---
+++ lib/data/models/hewan_model.dart
@@ -0,0 +1,19 @@
+class HewanModel {
+  final int id;
+  final String nama;
+  final String jenis;
+  final String deskripsi;
+  final int harga;
+  final String status;
+
+  HewanModel({
+    required this.id,
+    required this.nama,
+    required this.jenis,
+    required this.deskripsi,
+    required this.harga,
+    required this.status,
+  });
+
+  
+}
