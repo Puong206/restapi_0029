a6ac9d8e860a68b6cea10bfe620919c8cfbbf0dd
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-05-01 23:07:49 +0700

    feat: tambahkan metode untuk mengambil dan menghapus token JWT

--- lib/data/repositories/auth_repository.dart
+++ lib/data/repositories/auth_repository.dart
@@ -7,4 +7,12 @@ class AuthRepository {
   Future<void> persistToken(String token) async {
     await _storage.write(key: 'jwt_token', value: token);
   }
+
+  Future<String?> getToken() async {
+    return await _storage.read(key: 'jwt_token');
+  }
+
+  Future<void> deleteToken() async {
+    await _storage.delete(key: 'jwt_token');
+  }
 }
