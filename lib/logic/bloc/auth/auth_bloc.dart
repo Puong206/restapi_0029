826f3f2d399e6850487ffa9b50c2d45a0af22ffc
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-05-01 23:36:13 +0700

    feat: tambahkan kelas AuthBloc untuk menangani logika otentikasi

---
+++ lib/logic/bloc/auth/auth_bloc.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:restapi_0029/logic/bloc/auth/auth_event.dart';
+import 'package:restapi_0029/logic/bloc/auth/auth_state.dart';
+
+class AuthBloc extends Bloc<AuthEvent, AuthState>{
+  
+}
