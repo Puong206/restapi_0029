908db85f3c93739a1eb2a15c3402983a000fec88
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-05-01 23:28:16 +0700

    feat: tambahkan metode updateHewan untuk memperbarui data hewan

--- lib/data/repositories/hewan_repository.dart
+++ lib/data/repositories/hewan_repository.dart
@@ -42,4 +42,23 @@ class HewanRepository {
       throw data['message'] ?? "Gagal menambahkan hewan: ${response.statusCode}";
     }
   }
+
+  Future<void> updateHewan(int id, Map<String, dynamic> hewanData) async {
+    final token = await storage.getToken();
+
+    final response = await http.put(
+      Uri.parse("$_baseUrl/hewan/$id"),
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': 'Bearer $token',
+      },
+      body: jsonEncode(hewanData),
+    );
+
+    if (response.statusCode != 200) {
+      final data = jsonDecode(response.body);
+      throw data['message'] ?? "Gagal memperbarui data hewan: ${response.statusCode}";
+    }
+  }
 }
